64fefb68327a2ea7036fb341dd2c8724674454bc
Alex Murray <murray.alex@gmail.com>
2016-01-28 22:20:27 +1030

    Put test name in docstring of test case method
    Should then get shown on travis-ci

--- test_cstyle.py
+++ test_cstyle.py
@@ -8,16 +8,16 @@ import unittest
 
 class CStyleTestCase(unittest.TestCase):
     """Base test case for testing CStyle."""
-    def __init__(self, name, base_name, expected_errors):
-        self._base_name = base_name
+    def __init__(self, name, basename, expected_errors):
+        self._basename = basename
         self._expected_errors = (expected_errors if expected_errors is not None
                                  else [])
         super(CStyleTestCase, self).__init__(name)
 
     def runTest(self):
-        """Override runTest method with our own implementation."""
+        """Test {basename}.""".format(basename=self._basename)
         base = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                            'test', self._base_name)
+                            'test', self._basename)
         errors = cstyle.CStyle(base + '.conf',
                                [base + '.c']).check()
         self.assertEqual(errors, self._expected_errors)
@@ -30,8 +30,8 @@ class CStyleTestSuite(unittest.TestSuite):
                  '0002_pointer_prefix_repeat': [],
                  '0003_no_goto': [],
                  '0004_prefer_goto': []}
-        for (base_name, expected_errors) in tests.iteritems():
-            test = CStyleTestCase('runTest', base_name, expected_errors)
+        for (basename, expected_errors) in tests.iteritems():
+            test = CStyleTestCase('runTest', basename, expected_errors)
             self.addTest(test)
 
 if __name__ == '__main__':
